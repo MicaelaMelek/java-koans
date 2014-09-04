1d30a70eb426feed8ff6187434208b42b57d67b9
Micaela <micaelamelek@gmail.com>
2014-09-04 16:46:15 -0300

    Update0409

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -183,13 +183,13 @@ public class AboutPrimitives {
 
 	@Koan
 	public void floatsHaveASmallerRangeThanDoubles() {
-		assertEquals(Float.MIN_VALUE, 1.4E-45);
-		assertEquals(Float.MAX_VALUE, __);
+		assertEquals(Float.MIN_VALUE, 1.4E-45f);
+		assertEquals(Float.MAX_VALUE, 3.4028235E38f);
 	}
 
 	@Koan
 	public void floatSize() {
-		assertEquals(Float.SIZE, __);
+		assertEquals(Float.SIZE,32);
 	}
 
 	private Class<?> getType(int value) {
